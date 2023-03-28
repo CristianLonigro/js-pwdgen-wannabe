5e5b156ca2c27d4f545f540d3635e8c69e0649bd
CristianLonigro <cristianlonigro24@gmail.com>
2023-03-28 18:09:05 +0200

    Nome+Cognome+Colore preferito

---
+++ main.js
@@ -0,0 +1,12 @@
+
+const nome = prompt("Inserisci il tuo nome");
+console.log(nome);
+
+const cognome = prompt("Inserisci cognome");
+console.log(cognome);
+
+const ColorePreferito = prompt("Inserisci il tuo colore preferito");
+console.log(ColorePreferito);
+
+const nomeCognomeColorePreferito = nome + cognome + ColorePreferito;
+console.log(nomeCognomeColorePreferito);
